23ffb4c83c215d36d6418fcaa39fe8dcdf093bd5
Paulina <cherry3475@gmail.com>
2018-07-12 09:25:42 +0200

    add app versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
-  resources :todos do
-    resources :items
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  # module the controllers without affecting the URI
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :todos, only: :index
   end
-end
 
-Rails.application.routes.draw do
-  # [...]
+  # namespace the controllers without affecting the URI
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :todos do
+      resources :items
+    end
+  end
+
   post 'auth/login', to: 'authentication#authenticate'
-end
-
-Rails.application.routes.draw do
-  # [...]
   post 'signup', to: 'users#create'
 end
 
